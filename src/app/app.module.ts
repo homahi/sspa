030a5c68031ca0606a26603f8509e98d4ab64210
homahi <homahi0128@gmail.com>
2017-09-09 20:49:22 +0900

    トップページ画面を追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      [{
+        path: 'top',
+        component: TopComponent
+      }, {
+        path: '',
+        redirectTo: '/top',
+        pathMatch: 'full'
+      }]
+    ),
     BrowserModule
   ],
   providers: [],

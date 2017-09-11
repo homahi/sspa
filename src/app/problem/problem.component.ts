7905fa3fc198bfe9d6902eebb524085308890425
homahi <homahi0128@gmail.com>
2017-09-11 22:36:24 +0900

    問題を表示できるようにしできるようにした

--- src/app/problem/problem.component.ts
+++ src/app/problem/problem.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-problem',
   templateUrl: './problem.component.html',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProblemComponent implements OnInit {
 
-  constructor() { }
+  problems = [
+    {
+      description: 'What is truth?',
+      code: 'function problem() { return __;}'
+    },
+    {
+      description: 'Simple Math',
+      code: 'function problem() { return 42 === 6 * __;}'
+    }
+  ];
+  problem: any;
+  problemId: string;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.problemId = this.route.snapshot.paramMap.get('id');
+
+
   }
 
 }

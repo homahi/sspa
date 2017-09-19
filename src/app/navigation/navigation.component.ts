6ccd168def58271dbb10e958a81e2ea949f04e6a
homahi <homahi0128@gmail.com>
2017-09-19 19:14:42 +0900

    問題表示時にSkipと表示されるように修正

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
-
-  constructor() { }
+  path: any;
+  isShowSkip: boolean;
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe((event: NavigationEnd) => {
+        console.log(location.pathname);
+        this.isShowSkip = this.router.url.indexOf('problem') < 0 ? false : true;
+      });
   }
 
 }
